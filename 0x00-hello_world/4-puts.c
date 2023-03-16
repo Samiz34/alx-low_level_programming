f20a2e1e93e67d825bd52a3384d4e90f0721df5e
“Samuel” <samuelzeleke34@gmail.com>
2023-03-16 13:02:13 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
- * main - Programming is like building a multilingual puzzle
- * return - 0
+ * main - print Programming is like building a multilingual puzzle
+ * return:  0
  */
 
 int main(void)
