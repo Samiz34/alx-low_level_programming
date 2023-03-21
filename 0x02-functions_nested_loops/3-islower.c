7c44c8d426745cd796462d18ddfa8bfa45a5f5b9
“Samuel” <samuelzeleke34@gmail.com>
2023-03-21 12:47:38 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print alphabet 
+ * _islower - function to print lower case
+ * Return: always Zero
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+
+}
