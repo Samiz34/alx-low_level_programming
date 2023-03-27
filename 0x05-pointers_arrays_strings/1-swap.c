bb59f649cf4a09964d7770a6c3c35b6eb018a208
“Samuel” <samuelzeleke34@gmail.com>
2023-03-27 06:48:33 -0700

    swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - the parametes to be swaped
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: 0
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *a;
+	*a = *b;
+	*b = *c;
+
+}
