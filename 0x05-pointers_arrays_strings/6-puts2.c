f50d42fae75e6e0422e6afa4908381c3223500a9
“Samuel” <samuelzeleke34@gmail.com>
2023-03-27 11:34:50 -0700

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ *
+ * @str: parameter
+ *
+ * Return: zero
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+		i++;
+
+}
